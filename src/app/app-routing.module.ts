0ad9d07e5055042861baca2ef2d0eba80f91d229
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-07-16 17:56:59 -0400

    feat(reservation): add reservation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 // import { LoginComponent } from './components/login/login.component';
 // import { RegisterComponent } from './components/register/register.component';
-// import { ReservationComponent } from './components/reservation/reservation.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
 
 const routes: Routes = [
   // { path: 'login', component: LoginComponent },
   // { path: 'register', component: RegisterComponent },
-  // { path: 'reservation', component: ReservationComponent },
+  { path: 'reservation', component: ReservationComponent },
 ];
 
 @NgModule({
