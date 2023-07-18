72a84dc73c95c9454aebfa712e3bca0c3701bb17
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-07-18 15:40:57 -0400

    app(config): edit configurations

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: false,
+  fiAPI: '<FlightInfo API URL>',
+  fiToken: '<FlightInfo Request Header Token>',
+
+  auth0: {
+    domain: '<Auth0 Domain>',
+    clientId: '<Auth0 Client ID>',
+  },
+
+  firebase: {
+    apiKey: '<Firebase API Key>',
+    authDomain: '<Firebase Auth Domain>',
+    projectId: '<Firebase Project ID>',
+    storageBucket: '<Firebase Storage Bucket>',
+    messagingSenderId: '<Firebase Messaging Sender ID>',
+    appId: '<Firebase App ID>',
+    measurementId: '<Firebase Measurement ID>',
+  },
+};
