68e3a4de696ae8aa0593e90013100926af0dad0c
Billy McCrackin <44419353+bmc1995@users.noreply.github.com>
2023-07-18 15:41:47 -0400

    feat(app): routing and auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { LoginComponent } from './components/login/login.component';
-// import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
-  // { path: 'login', component: LoginComponent },
-  // { path: 'register', component: RegisterComponent },
-  { path: 'reservation', component: ReservationComponent },
+  {
+    path: '',
+    component: ReservationComponent,
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
+import { NgbModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment } from '@environment';
+
+const { auth0 } = environment;
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReservationComponent
-  ],
+  declarations: [AppComponent, ReservationComponent, LoginComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    NgbToastModule,
+    AuthModule.forRoot({
+      domain: auth0.domain,
+      clientId: auth0.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
